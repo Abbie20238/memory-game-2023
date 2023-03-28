cf6f54502ce3027ec042e24d9e8cd7051bb91b59
Abbie20238 <128933255+Abbie20238@users.noreply.github.com>
2023-03-28 13:20:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -55,8 +55,7 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.showString("3:A+B")
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showString("nope")
-    basic.showIcon(IconNames.Sad)
+	
 })
 input.onPinPressed(TouchPin.P2, function () {
     basic.showArrow(ArrowNames.North)
@@ -115,45 +114,43 @@ input.onPinPressed(TouchPin.P2, function () {
     basic.showString("3:A+B")
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("try again")
-    basic.showIcon(IconNames.Sad)
+	
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("Right good memory")
-    basic.showIcon(IconNames.Happy)
+	
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.East)
     basic.pause(100)
-    basic.showArrow(ArrowNames.SouthEast)
-    basic.pause(100)
-    basic.showArrow(ArrowNames.SouthWest)
-    basic.pause(100)
-    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.South)
     basic.pause(100)
     basic.showArrow(ArrowNames.West)
+    basic.pause(100)
+    basic.showArrow(ArrowNames.North)
+    basic.pause(100)
+    basic.showArrow(ArrowNames.NorthEast)
     basic.pause(100)
     basic.clearScreen()
     basic.showNumber(1)
     basic.pause(900)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(350)
     basic.showArrow(ArrowNames.South)
     basic.pause(350)
-    basic.showArrow(ArrowNames.SouthWest)
-    basic.pause(350)
-    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.West)
     basic.pause(350)
     basic.showArrow(ArrowNames.North)
     basic.pause(350)
-    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthEast)
     basic.pause(350)
     basic.clearScreen()
     basic.showNumber(2)
     basic.pause(900)
-    basic.showArrow(ArrowNames.North)
-    basic.pause(350)
-    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.East)
     basic.pause(350)
     basic.showArrow(ArrowNames.SouthWest)
+    basic.pause(350)
+    basic.showArrow(ArrowNames.West)
     basic.pause(350)
     basic.showArrow(ArrowNames.NorthEast)
     basic.pause(350)
@@ -166,11 +163,11 @@ input.onPinPressed(TouchPin.P1, function () {
     basic.pause(350)
     basic.showArrow(ArrowNames.South)
     basic.pause(350)
-    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.NorthWest)
+    basic.pause(350)
+    basic.showArrow(ArrowNames.East)
     basic.pause(350)
     basic.showArrow(ArrowNames.North)
-    basic.pause(350)
-    basic.showArrow(ArrowNames.West)
     basic.pause(350)
     music.stopAllSounds()
     basic.clearScreen()
@@ -182,4 +179,24 @@ input.onLogoEvent(TouchButtonEvent.Released, function () {
     for (let index = 0; index < 6; index++) {
         music.playMelody("C5 B C5 G E F A D ", 120)
     }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        while (input.buttonIsPressed(Button.A)) {
+            basic.showString("Right good memory")
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
 })
